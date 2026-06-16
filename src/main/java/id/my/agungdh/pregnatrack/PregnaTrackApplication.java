6baf1c9c0f346a11a4e6e47c6c972660aa4eab94
AgungDH <agungdh@live.com>
2026-06-16 13:39:42 +0700

    init

---
+++ src/main/java/id/my/agungdh/pregnatrack/PregnaTrackApplication.java
@@ -0,0 +1,13 @@
+package id.my.agungdh.pregnatrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PregnaTrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PregnaTrackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/agungdh/pregnatrack/PregnaTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.agungdh.pregnatrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PregnaTrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
