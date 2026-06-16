161f5748f757abe80dd4cfde877bd1ec6d5ad3d1
AgungDH <agungdh@live.com>
2026-06-16 13:50:54 +0700

    swagger security config

---
+++ src/main/java/id/my/agungdh/pregnatrack/config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package id.my.agungdh.pregnatrack.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "PregnaTrack API",
+                version = "1.0",
+                description = "API Dokumentasi untuk Aplikasi Pelacak Kesehatan Bumil"
+        ),
+        // Mengaktifkan pengamanan global di dokumentasi, namun tetap bypass halaman swaggernya sendiri
+        security = @SecurityRequirement(name = "BearerAuth")
+)
+@SecurityScheme(
+        name = "BearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class OpenApiConfig {
+}
